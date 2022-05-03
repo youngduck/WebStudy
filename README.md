2ea4674d670e392bb22967c3f2749c2c013bdcef
youngduck <dudejr5839@naver.com>
2022-05-03 17:42:01 +0900

    cloneCoding:modal_popup

--- README.md
+++ README.md
@@ -11,26 +11,27 @@
 ![](./cloneCoding/bulls%20and%20cows/assets/images/result.gif)
 
 ## 드럼만들기
-### 키값에따른 반응형 웹만들기
+### 키값에따른 반응형 웹
 ![](./cloneCoding/Drum/assets/images/result.gif)
 
-## 스로틀을활용한 프로그레스바 만들기
+## 스로틀을활용한 프로그레스바
 ![](./cloneCoding/progressbar/assets/images/result.gif)
 
-## fetchAPI,pagenation 활용한 무한스크롤 웹만들기
+## fetchAPI,pagenation 활용 무한스크롤 웹
 ![](./cloneCoding/infinityScroll/assets/images/result.gif)
 
-## 캐러셀 만들기
+## 캐러셀
 ![](./cloneCoding/carousel/assets/images/result.gif)
 
-## 유튜브 만들기
+## 유튜브
 ![](./cloneCoding/youtube/assets/images/result.gif)
 
-## html게임만들기
+## html게임(snake)
 ### canvas,정규표현식,localStorage 활용
 ![](./cloneCoding/htmlGame//assets/images/result.gif)
 
-
+## 모달팝업
+![](./cloneCoding/modal_popup//assets/images/result.gif)
 # toyProject
 ## schoolSeatData 크롤링
 ### 학교도서관 좌석정보 크롤링 후 좌석사용여부 건네주기
--- cloneCoding/modal_popup/assets/js/index.js
+++ cloneCoding/modal_popup/assets/js/index.js
@@ -2,76 +2,29 @@ const get = (target)=>{
     return document.querySelector(target)
 }
 
-let currentPage = 1
-let total = 10
-const limit = 10
-const end = 100
 
-const $posts = get('.posts')
-const $loader = get('.loader')
+const body = get('body')
+const open_btn=get('.modal_open_button')
+const modal = get('.modal')
+const confirm = get('.modal_button.confirm')
+const cancel = get('.modal_button.cancel')
 
-const hideLoader = () => {
-    $loader.classList.remove('show')
+
+const toggleView =()=>{
+    modal.classList.toggle('show')
+    body.classList.toggle('scroll-lock')
 }
 
-const showLoader = () => {
-    $loader.classList.add('show')
-}
+open_btn.addEventListener('click',()=>toggleView())
 
-const showPosts = (posts)=>{
-    posts.forEach((post)=>{
-        const $post = document.createElement('div')
-        $post.classList.add('post')
-        $post.innerHTML=`
-        <div class="header">
-          <div class="id">${post.id}.</div>
-          <div class="title">${post.title}</div>
-        </div>
-        <div class="body">${post.body}</div>
-    `
-        $posts.appendChild($post)
-    })
+confirm.addEventListener('click',()=>toggleView())
 
-}
+cancel.addEventListener('click',()=>toggleView())
 
-const getPosts = async(page,limit)=>{
-    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`
-    const response = await fetch(API_URL)
-    if(!response.ok){
-        throw new Error('에러가 발생했습니다.')
-    }
-    return await response.json()
-}
 
-const loadPosts= async (page,limit)=>{
-    showLoader()
-    try {
-        const response = await getPosts(page,limit)
-        showPosts(response)
-    } catch (error){
-        console.error(error.message)
-    } finally {
-        hideLoader()
-    }
-} 
-
-const handleScroll = ()=>{
-    const {scrollTop,scrollHeight,clientHeight} = document.documentElement
-
-    if(total === end){
-        window.removeEventListener('scroll',handleScroll)
-        return
+window.addEventListener('click',(e)=>{
+    if(e.target===modal){
+        toggleView()
     }
 
-    if(scrollTop+clientHeight>=scrollHeight-5){
-        currentPage++
-        total+=10
-        loadPosts(currentPage,limit)
-        return
-    }
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    loadPosts(currentPage, limit)
-    window.addEventListener('scroll', handleScroll)
-  })
+})
